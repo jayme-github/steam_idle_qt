4d8c379a367bfc310867bf7812e6de295c4d9b4d
jayme-github <tuxnet@gmail.com>
2016-03-03 23:01:00 +0100

    Move steam_idle_qt into separate repo

--- setup.py
+++ setup.py
@@ -6,41 +6,31 @@ import py2exe
 import PyQt4
 import requests.certs
 
-print(PyQt4.__path__)
-
 def parse_requirements(path):
     with open(path, 'r') as infile:
         return [l.strip() for l in infile.readlines()]
 
 setup(
-    name = 'steam_idle',
+    name = 'steam_idle_qt',
     version = '0.1',
     description = 'Idle your Steam libraty for cards',
     author = 'Jayme',
     author_email = 'tuxnet@gmail.com',
-    url = 'https://github.com/jayme-github/steam_idle',
+    url = 'https://github.com/jayme-github/steam_idle_qt',
     install_requires = parse_requirements('requirements.txt'),
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    packages = [
-        'steam_idle',
-        'steam_idle_qt'
-    ],
+    packages = ['steam_idle_qt'],
     data_files = [
-        ('libs', ['steam_idle/libs/steam_api64.dll',
-                    'steam_idle/libs/libsteam_api.so',
-                    'steam_idle/libs/steam_api.dll',
-                    'steam_idle/libs/libsteam_api64.so',
-                    'steam_idle/libs/libsteam_api.dylib']),
         ('', [requests.certs.where(),]),
+        ('', ['NoImage.png',]),
         ('imageformats', [os.path.join(PyQt4.__path__[0], 'plugins', 'imageformats', 'qjpeg4.dll'),]),
     ],
     scripts = [
-        'steam_idle_cli.py',
         'steam_idle_gui.py',
     ],
     windows = ['steam_idle_gui.py'],
